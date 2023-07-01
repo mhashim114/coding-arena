4f894635239288d6355865c6f244a422321cb971
muhammad.hashim@dastgyr.com <muhammad.hashim@dastgyr.com>
2023-07-01 18:44:01 +0500

    added third solution

--- algoExpert/easy/twoNumberSum.ts
+++ algoExpert/easy/twoNumberSum.ts
@@ -23,3 +23,23 @@ function twoNumberSum2(array: number[], targetSum: number) {
     }
     return [];
 }
+
+
+function twoNumberSum3(array: number[], targetSum: number) {
+    let firstPointer = 0;
+    let secondPointer = array.length - 1;
+    array = array.sort(function (a, b) { return a - b });
+
+    while (firstPointer <= secondPointer) {
+        if (array[firstPointer] + array[secondPointer] === targetSum) {
+            return [array[firstPointer], array[secondPointer]];
+        }
+        else if (array[firstPointer] + array[secondPointer] < targetSum) {
+            firstPointer += 1;
+        }
+        else if (array[firstPointer] + array[secondPointer] > targetSum) {
+            secondPointer -= 1;
+        }
+    }
+    return [];
+}
